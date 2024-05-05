a8033adcb77aacf53d9c3939ad0908037442d68a
vivi-codesalot <nguy.vivian.vi@outlook.com>
2024-05-05 15:59:06 -0400

    implement: navigation, integration with pages to navigation bar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,38 +1,9 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import NavBar from "../components/navigation"
 
 export default function Root() {
     return (
         <>
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container fluid>
-                    <Navbar.Brand href="#">t4tclt</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav
-                            className="me-auto my-2 my-lg-0"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll
-                        >
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#about_us">About Us</Nav.Link>
-                            <Nav.Link href="#events_calendar">Events Calendar</Nav.Link>
-                            <NavDropdown title="Resources" id="navbarScrollingDropdown">
-                                <NavDropdown.Item href="#action4">
-                                    Placeholder 1
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action5">
-                                    Placeholder 2
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+        <NavBar/>
         </>
     );
 }
